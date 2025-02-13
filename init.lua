4fcde04d83efc0980aabbe3096e73a818b25895e
GreenCaleb3 <caleb.beichner3@gmail.com>
2025-02-13 12:58:28 -0500

    Best config so far

--- init.lua
+++ init.lua
@@ -3,6 +3,9 @@ require("config.lazy")
 vim.opt.shiftwidth = 4
 vim.opt.clipboard = "unnamedplus"
 
+vim.opt.number = true
+vim.opt.relativenumber = true
+
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
